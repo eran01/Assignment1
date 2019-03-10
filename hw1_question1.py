b96192707a1189fa8de95c818aaba9839ee657e4
eran01 <48094145+eran01@users.noreply.github.com>
2019-03-10 19:54:54 +0200

    Update hw1_question1.py

--- hw1_question1.py
+++ hw1_question1.py
@@ -1,7 +1,7 @@
 def trifeca(word):
     m = False
     if len(word)<6:
-        return False
+        return m
     if len(word)==6:
         if word[0]==word[1] and word[2]==word[3] and word[4]==word[5]:
             m=True
